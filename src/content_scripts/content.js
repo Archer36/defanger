a753d8e7ad7f8e2cc79002718788cfb1b9317e44
Archer36 <archer36@gmail.com>
2023-11-14 15:54:44 -0500

    RegEx and Functional Updates
    Made port number optional in RegEx.
    Workaround to allow reading of selected data in TEXTAREA.
    In the URL regex replaced // with \/. I think was was a typo to include the forward slash which needs to be escaped, I think.
    Included the whitespace character int he URL regex to match URLs with spaces.
    Updated RegEx to match on port numbers in IPs and domains as well.

--- src/content_scripts/content.js
+++ src/content_scripts/content.js
@@ -1,17 +1,27 @@
 document.addEventListener("selectionchange", ()=> {
     indicatorParser = new IndicatorParser();
 
-    let selectedText = document.getSelection().toString().trim();
+  let selectedText;
+  if (document.activeElement.tagName === 'TEXTAREA') {
+    // https://stackoverflow.com/a/74602959 - getSelection() doesn't work in TEXTAREAS
+    // console.log("TEXTAREA selection");
+    selectedText = document.activeElement.value.substring(
+      document.activeElement.selectionStart,
+      document.activeElement.selectionEnd
+    );
+  } else {
+    // console.log("Normal selection")
+    selectedText = document.getSelection().toString().trim();
+  }
+  console.log(selectedText);
 
-    if (selectedText) {
-        type = indicatorParser.getIndicatorType(selectedText);
-        browser.runtime.sendMessage({
-            id: 0,
-            type: type,
-            indicator: selectedText
-        });
-
-    }
+  if (selectedText) {
+    type = indicatorParser.getIndicatorType(selectedText);
+    browser.runtime.sendMessage({
+      id: 0,
+      type: type,
+      indicator: selectedText});
+  }
 });
 
 
